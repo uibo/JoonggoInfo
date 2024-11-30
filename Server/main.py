403e3fb4afb53d74ed28b2db8b0e445d89835516
uibo <boui000210@gmail.com>
2024-11-30 16:07:04 +0900

    delete file except for Client

--- Server/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-from Domain.DB.iPhone14_processed_info.router import router as iPhone14_processed_info_router
-from Domain.Business.router import router as Business_router
-
-app = FastAPI()
-app.include_router(iPhone14_processed_info_router)
-app.include_router(Business_router)
